79bb1da7e0c8439472c6293b3b177171ac114508
abel <vnponce8@gmail.com>
2023-12-02 23:20:30 -0600

    refactor(socialapi): Posts router and models

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
 from fastapi import FastAPI
+from routers.post import router as post_routers
 
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+app.include_router(post_routers)
