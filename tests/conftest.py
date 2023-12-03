a80e6af414df7d4f1d4afecd2c81473142b0168e
abel <vnponce8@gmail.com>
2023-12-03 16:20:48 -0600

    chore(data-base): test environment by `os.environ`

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,21 +1,27 @@
+import os
 from typing import AsyncGenerator, Generator
 
 import pytest
 
 from fastapi.testclient import TestClient
 from httpx import AsyncClient
-
-from main import app
 from routers.post import comment_table, post_table
+
+os.environ["ENV_STATE"] = "test"  # to run test in "test" mode
+
+from main import app  # noqa: E402 <- avoid linter to sent this line above the "os.envirion"
+
 
 # This means thest will run once per session
 @pytest.fixture(scope="session")
 def anyio_backend():
     return "asyncio"
 
+
 @pytest.fixture()
 def client() -> Generator:
     yield TestClient(app)
+
 
 @pytest.fixture(autouse=True)
 async def db() -> Generator:
